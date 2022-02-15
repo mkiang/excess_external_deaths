fb2bdb82e3a27fd77093457f9cad6d1b25ea106b
Mathew Kiang <mathew.kiang@gmail.com>
2022-02-15 11:04:11 -0800

    Reorder fig1 as requested

--- code/03_figure1.R
+++ code/03_figure1.R
@@ -16,36 +16,91 @@ MAIN_CODS <- c(
 ## Data ----
 analytic_df <- readRDS(here("data", "excess_mortality_estimates.RDS"))
 
-plot_df <- analytic_df %>% 
+plot_df <- analytic_df %>%
     filter(date == max(date),
-           cause_of_death %in% MAIN_CODS) %>% 
-    mutate(cause_of_death_cat = factor(cause_of_death,
-                                       levels = rev(MAIN_CODS),
-                                       ordered = TRUE))
+           cause_of_death %in% MAIN_CODS) %>%
+    mutate(cause_of_death_cat = factor(
+        cause_of_death,
+        levels = rev(MAIN_CODS),
+        ordered = TRUE
+    )) %>%
+    mutate(race_cat = factor(
+        race_eth,
+        levels = rev(
+            c(
+                "American Indian or Alaska Native",
+                "Asian or Pacific Islander",
+                "Non-Hispanic Black",
+                "Hispanic",
+                "Non-Hispanic White",
+                "All"
+            )
+        ),
+        labels = rev(
+            c(
+                "American Indian or Alaska Native",
+                "Asian or Pacific Islander",
+                "Black",
+                "Hispanic",
+                "White",
+                "Total population"
+            )
+        ),
+        ordered = TRUE
+    ))
 
 ## Plot ----
-p1 <- ggplot(plot_df,
+p1 <- ggplot(plot_df %>% 
+                 filter(race_eth != "All"),
        aes(x = cumulative_mean / pop * 100000,
            xmin = cumulative_lower / pop * 100000,
            xmax = cumulative_upper / pop * 100000,
            y = cause_of_death_cat,
-           color = race_eth,
-           group = race_eth,
-           size = cumulative_mean)) + 
+           color = race_cat,
+           group = race_cat,
+           size = abs(cumulative_mean))) + 
     geom_vline(xintercept = 0,
                alpha = .5) + 
-    geom_point(position = position_dodge2(width = .3)) + 
+    geom_point(position = position_dodge2(width = .3),
+               alpha = .8) + 
     geom_errorbarh(position = position_dodge2(width = .3),
                    size = .7, 
-                   height = .3) + 
+                   height = .3,
+                   alpha = .8) + 
+    scale_size_binned_area("Absolute number\nof excess deaths",
+                           breaks = (c(100, 500, 1000, 5000)),
+                           labels = function(x) sprintf("%i", round(x))) + 
+    scale_color_jama(name = "Race and Ethnicity", ) + 
+    scale_x_continuous("Total excess deaths per 100,000 population (95% PI)") + 
+    scale_y_discrete("Type of death") + 
+    mk_nytimes(legend.position = "right") + 
+    guides(color = guide_legend(reverse = TRUE))
+
+## Plot ----
+p1_total <- ggplot(plot_df,
+             aes(x = cumulative_mean / pop * 100000,
+                 xmin = cumulative_lower / pop * 100000,
+                 xmax = cumulative_upper / pop * 100000,
+                 y = cause_of_death_cat,
+                 color = race_cat,
+                 group = race_cat,
+                 size = abs(cumulative_mean))) + 
+    geom_vline(xintercept = 0,
+               alpha = .5) + 
+    geom_point(position = position_dodge2(width = .3),
+               alpha = .8) + 
+    geom_errorbarh(position = position_dodge2(width = .3),
+                   size = .7, 
+                   height = .3,
+                   alpha = .8) + 
     scale_size_binned_area("Absolute number\nof excess deaths",
                            breaks = (c(100, 500, 1000, 5000, 10000)),
                            labels = function(x) sprintf("%i", round(x))) + 
     scale_color_jama(name = "Race and Ethnicity", ) + 
     scale_x_continuous("Total excess deaths per 100,000 population (95% PI)") + 
     scale_y_discrete("Type of death") + 
-    mk_nytimes(legend.position = "right")
-    
+    mk_nytimes(legend.position = "right") + 
+    guides(color = guide_legend(reverse = TRUE))
 
 ## Save ----
 ggsave(
@@ -58,6 +113,22 @@ ggsave(
 )
 ggsave(
     here("plots", "figure1.jpg"),
+    p1,
+    width = 10,
+    height = 5,
+    scale = 1,
+    dpi = 300
+)
+ggsave(
+    here("plots", "figure1_total.pdf"),
+    p1,
+    width = 10,
+    height = 5,
+    scale = 1,
+    device = cairo_pdf
+)
+ggsave(
+    here("plots", "figure1_total.jpg"),
     p1,
     width = 10,
     height = 5,
